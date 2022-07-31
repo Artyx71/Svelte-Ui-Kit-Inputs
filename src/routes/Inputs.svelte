5493cd06a375b5b37e5a2fcb26d3d454e83e4d28
Andrew Gabaraev <androgabar@gmail.com>
2022-07-31 22:25:09 +0300

    Ui-kit-inputs

---
+++ src/routes/Inputs.svelte
@@ -0,0 +1,43 @@
+<script>
+	let showText = 'Заголовок';
+	export let hide;
+	export let mail;
+	export let leftIcon;
+	export let rightIcon;
+</script>
+
+<main class="input">
+	<span class="input__title {hide}">{showText}</span>
+	<div class="input__wrap">
+		<img class={leftIcon ? 'true' : 'hide'} src={leftIcon} alt="#" />
+		<input class="input__enter" type="text" placeholder="Текст" />
+		<img class={mail ? 'true' : 'hide'} src={mail} alt="#" />
+		<button class="input__button {rightIcon ? 'true' : 'hide'}"
+			><img src={rightIcon} alt="#" /></button
+		>
+	</div>
+</main>
+
+<style>
+	.hide {
+		display: none;
+	}
+
+	.input__enter {
+		width: 100%;
+		border: none;
+		outline: none;
+		background: #c9ebfc;
+		height: 100%;
+	}
+
+	.input__wrap {
+		display: flex;
+		align-items: center;
+		width: 424px;
+		border: 1px solid #c9ebfc;
+		border-radius: 6px;
+		padding: 16px;
+		background-color: #c9ebfc;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Inputs from './inputs.svelte';
+	let bank = '/Visa.svg';
+	let mail = '/mail.svg';
+	let search = '/search.svg';
+</script>
+
+<Inputs leftIcon={bank} rightIcon={search} {mail} />
+<Inputs leftIcon={bank} />
+<Inputs rightIcon={search} />
+<Inputs {mail} />
+
+<style></style>
